13427d1618032c8b9066a5f2631f2a6b33845918
Kanchan Jeswani <kanchanjeswani2000@gmail.com>
2020-08-09 18:50:33 +0530

    Problem A

---
+++ RainbowDashFluttershyandChessColoringProblemA.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//using namespace boost::multiprecision;
+#define lli long long int 
+#define endl '\n'
+#define boost  ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+#define pb push_back
+#define mod 1000000007
+#define p_q priority_queue
+#define fi first
+#define se second
+#define ub upper_bound
+#define lb lower_bound
+#define maxi max_element
+#define mini min_element
+using namespace std;
+int main(){
+    boost;
+   int t;
+    cin>>t;
+    while(t--){
+    int n;
+    cin>>n;
+    cout<<(n/2+1)<<endl;  
+   }
+}
+//TC: O(1)
+//SC: O(1)
+
+
